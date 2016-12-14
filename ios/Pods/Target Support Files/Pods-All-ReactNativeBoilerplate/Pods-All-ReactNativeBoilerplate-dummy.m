c797c213407f0952084ffa13e6869f21319f8f24
bobwei <bob61016@gmail.com>
2016-12-15 01:24:27 +0800

    rm Pods/

--- ios/Pods/Target Support Files/Pods-All-ReactNativeBoilerplate/Pods-All-ReactNativeBoilerplate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_All_ReactNativeBoilerplate : NSObject
-@end
-@implementation PodsDummy_Pods_All_ReactNativeBoilerplate
-@end
--- ios/Pods/Target Support Files/Pods-All-ReactNativeBoilerplateTests/Pods-All-ReactNativeBoilerplateTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_All_ReactNativeBoilerplateTests : NSObject
-@end
-@implementation PodsDummy_Pods_All_ReactNativeBoilerplateTests
-@end
