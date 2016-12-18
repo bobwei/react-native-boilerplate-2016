c35363661f1f354a4793c1eeaeb4fd098fea51f4
bobwei <bob61016@gmail.com>
2016-12-18 17:42:47 +0800

    add redux-persist

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 
 import rootReducer from '../reducers';
 
@@ -8,11 +10,19 @@ export default (initialState) => {
 
   const enhancers = [applyMiddleware(...middlewares)];
 
+  enhancers.push(autoRehydrate());
+
   const store = createStore(
     rootReducer,
     initialState,
     compose(...enhancers),
   );
+
+  persistStore(store, { whitelist: ['user'], storage: AsyncStorage }, () => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('state autoRehydrate completed');
+    }
+  });
 
   return store;
 };
